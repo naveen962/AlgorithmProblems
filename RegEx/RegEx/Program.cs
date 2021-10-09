ab0e07a1eab98539e9df68956953c0e219b7a93e
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-10-09 04:37:18 -0700

    Regular expression program is added

---
+++ RegEx/RegEx/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RegEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Message Demo");
+            DemoMessage.Display();
+        }
+    }
+}
