f32fd186dea752e4c9998e8da90fc74fc785f591
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-10-09 04:15:11 -0700

    Bubble sort program is added

---
+++ BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to BUBBLE SORT");
+            int[]a = { 36, 19, 29, 12, 74, 6 };
+
+            for(int i = 0; i < a.Length; i++)
+            {
+                for (int j = i; j < a.Length ; j++)
+                {
+                    if (a[j] < a[i])
+                    {
+                        int temp = a[j];
+                        a[j] = a[i];
+                        a[i] = temp;
+                    }
+                }
+
+            }
+            Console.WriteLine("Array after sorting");
+            for (int i = 0; i <= a.Length - 1; i++)
+            {
+                Console.Write(a[i] + " ");
+            }
+
+        }
+    }
+}
