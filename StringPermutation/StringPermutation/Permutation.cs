483d9180e9c81c6fcdd77ebae52872f7fb9c190b
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-10-09 04:08:49 -0700

    program to find the permutation of a string is added

---
+++ StringPermutation/StringPermutation/Permutation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringPermutation
+{
+    class Permutation
+    {
+        public static void Permute(String s, int startIndex, int endIndex)
+        {
+            if (startIndex == endIndex)
+            {
+                Console.WriteLine(s);
+
+            }
+            else
+            {
+                for (int i = startIndex; startIndex < endIndex; startIndex++)
+                {
+                  s=  Swaping(s, startIndex, i);
+                    Permute(s, startIndex + 1, endIndex);
+                  s=  Swaping(s, startIndex, i);
+                }
+            }
+
+        }
+        public static String Swaping(String s, int i, int j)
+        {
+            char temp;
+            char[] charArr = s.ToCharArray();
+            temp = charArr[i];
+            charArr[i] = charArr[j];
+            charArr[j] = temp;
+            string resultString = new string(charArr);
+
+            return resultString;
+        }
+    }
+}
---
+++ StringPermutation/StringPermutation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StringPermutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to STRING PERMUTATIONS");
+            Console.WriteLine("Enter the String");
+            string s = Console.ReadLine();
+            int n = s.Length;
+            Permutation.Permute(s, 0, n - 1);
+        }
+    }
+}
