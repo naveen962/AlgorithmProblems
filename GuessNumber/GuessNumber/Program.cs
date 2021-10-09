5028e20bec9c5cfd8d1b1f0f1058fce771fb5ecb
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-10-09 04:33:41 -0700

    guess no program is added

---
+++ GuessNumber/GuessNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GuessNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Guess.ThinkNumber();
+        }
+    }
+}
