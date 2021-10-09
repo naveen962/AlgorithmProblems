814ce9f2a0f450fcdb30101695540fe80b0f27b8
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-10-09 04:18:13 -0700

    Merge sort program is added

---
+++ MergeSort/MergeSort/Merge.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSort
+{
+    class Merge
+    {
+        public static void Merging(string[] arr, int l, int m, int r)
+        {
+            int n1 = m - l + 1;
+            int n2 = r - m;
+            string[] left = new string[n1];
+            string[] right = new string[n2];
+            int i, j;
+            for (i = 0; i < n1; ++i)
+            {
+                left[i] = arr[l + i];
+            }
+            for (j = 0; j < n2; ++j)
+            {
+                right[j] = arr[m + 1 + j];
+            }
+
+            i = 0;
+            j = 0;
+            int k = l;
+            while (i < n1 && j < n2)
+            {
+                if (left[i].CompareTo( right[j])<=0)
+                {
+                    arr[k] = left[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = right[j];
+                    j++;
+                }
+                k++;
+            }
+            while (i < n1)
+            {
+                arr[k] = left[i];
+                i++;
+                k++;
+            }
+            while (j < n2)
+            {
+                arr[k] = right[j];
+                j++;
+                k++;
+            }
+        }
+        //Sorting method
+        public static void Sort(string[] arr, int l, int r)
+        {
+            if (l < r)
+            {
+
+                int m = (l + r) / 2;
+                Sort(arr, l, m);
+                Sort(arr, m + 1, r);
+                Merging(arr, l, m, r);
+            }
+        }
+        public static void PrintArray(string[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i)
+                Console.Write(arr[i] + " ");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ MergeSort/MergeSort/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+namespace MergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To MERGE SORT");
+            string[] arr = { "ram", "abhi", "billy","Martin","sachin", "virat" };
+            Merge.Sort(arr, 0, arr.Length - 1);
+            Merge.PrintArray(arr);
+        }
+    }
+}
