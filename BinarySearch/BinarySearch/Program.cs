96d48e3cdc3d46c20c796a299a8e4149f28628d2
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-10-09 04:10:38 -0700

    Binary search program is added

---
+++ BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string arg = "Welcome to tech hub India";
+            var arr = arg.Split(" ");
+            Console.WriteLine("Enter word to search");
+            string word = Console.ReadLine();
+            Search bs = new Search(arr, word);
+            bs.BinarySearch(0, arr.Length - 1);
+        }
+    }
+}
---
+++ BinarySearch/BinarySearch/Search.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearch
+{
+
+
+    class Search
+    {
+        private string[] strArr;
+        private string word;
+        public Search(string[] arr, string word)
+        {
+            this.strArr = arr;
+            this.word = word;
+            Array.Sort(this.strArr);
+        }
+
+        public void BinarySearch(int startIndex, int endIndex)
+        {
+            int midIndex = (startIndex + endIndex) / 2;
+            while (startIndex <= endIndex)
+            {
+                if (strArr[midIndex].Equals(word))
+                {
+                    Console.WriteLine("{0} found", word);
+                    break;
+                }
+                else if (strArr[midIndex].CompareTo(word) < 0)
+                {
+                    startIndex = midIndex + 1;
+                }
+                else
+                {
+                    endIndex = midIndex - 1;
+                }
+                midIndex = (startIndex + endIndex) / 2;
+            }
+            if (startIndex > endIndex)
+            {
+                Console.WriteLine("Element not found");
+            }
+
+        }
+
+    }
+
+        
+}
