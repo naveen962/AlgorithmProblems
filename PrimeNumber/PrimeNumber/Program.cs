9320fc7ababa83744cd57fcb403b0092a46cc097
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-10-09 04:22:05 -0700

    Prime no between 1-1000 program is added

---
+++ PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1000;
+            int i, j;
+            int temp = 0;
+
+            for ( i = 2; i < n; i++)
+            { 
+
+                for (j = 2; j < i; j++)
+                {
+                    if ((i % j) == 0)
+                    {
+                        temp = temp + 1;
+                        break;
+                    }
+                }
+                if (temp == 0)
+                {
+                  Console.WriteLine(i);
+                }
+                else
+                {
+                   temp = 0;
+                }
+            }
+                
+            
+        }
+    }
+}
